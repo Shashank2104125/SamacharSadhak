9e7b2e20d48c41af71e46ec5876354dbd36e0b2e
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2024-02-06 19:25:42 +0530

    added more intents with updated home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React,{useState,useEffect} from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from "./components/NewsCards/NewsCards";
+import Data from "./data.json"
 const alankey='7446476298353e62bad05faab7ce37db2e956eca572e1d8b807a3e2338fdd0dc/stage'
 const App=()=>{
     const [newsArticles,setNewsArticles]=useState([]);
     useEffect(()=>{
      alanBtn({
         key:alankey,
-        // onCommand:({command,name})=>{
-        //     if(command==="testCommand"){
-        //         console.log(name)
-        //     }
-        // },
         onCommand:({command,articles})=>{
             if(command==='newHeadLines'){
                  console.log(articles)
@@ -22,7 +18,9 @@ const App=()=>{
     },[])
     return(
         <div>
-            <h1>Alan ai Application</h1>
+            <div style={{textAlign:"center",padding:"5rem"}}>
+            <img style={{width:"15rem",margin:"auto"}} src="https://assets-global.website-files.com/64ec3fc5bb945b48c0a37b1c/64f5aad0cbdbf0b7d248f4aa_alan%20ai_1.webp"  alt="logo" />
+            </div>
             <NewsCards articles={newsArticles}/>
         </div>
     )
